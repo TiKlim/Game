483aafaf87b5973b1daa074c53306d6cc7b876da
Klim <114877716+TiKlim@users.noreply.github.com>
2023-12-28 02:37:11 +0300

    Add files via upload

---
+++ Game3/Game3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Game3;
+namespace Game3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Game.persons = new List<Game>();
+            Console.WriteLine("");
+            Console.WriteLine("                                                       ~           ");
+            Console.WriteLine("                                                INSPECTOR MORS");
+            Console.WriteLine("                                                the  videogame");
+            Console.WriteLine("                                                       ~           ");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("                                           press ENTER,  чтобы начать.");
+            Console.ReadLine();
+            Game.Menu(Game.persons);
+        }
+    }
+}
